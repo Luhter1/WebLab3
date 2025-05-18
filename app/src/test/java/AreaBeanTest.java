da7e52b2a08b98b5c3b176ab547b71c542cf3ecb
Luhter1 <kaparulinti@mail.ru>
2025-05-18 21:45:22 +0300

    add test target

--- app/src/test/java/AreaBeanTest.java
+++ app/src/test/java/AreaBeanTest.java
@@ -1,3 +1,25 @@
+import org.junit.jupiter.api.Test;
+import org.itmo.manageBean.AreaChecker;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class AreaBeanTest {
-    
+
+    @Test
+    public void testCheckHitFirstQuadrant() {
+        AreaChecker bean = new AreaChecker();
+        assertTrue(bean.IsHit(-1,1, 4));
+    }
+
+    @Test
+    public void testCheckHitSecondQuadrant() {
+        AreaChecker bean = new AreaChecker();
+        assertTrue(bean.IsHit(-0.5, -0.5, 2));
+    }
+
+    @Test
+    public void testCheckHitThirdQuadrant() {
+        AreaChecker bean = new AreaChecker();
+        assertTrue(bean.IsHit(2, -1, 3));
+    }
 }
--- start.sh
+++ start.sh
@@ -1,6 +1,1 @@
-# gradle build
-# mv ./app/build/libs/server.war ./wildfly/standalone/deployments/server.war
-# ./wildfly/bin/standalone.sh
-
-# gradle build
-# sudo docker compose up --build -d
+sudo docker compose up --build
